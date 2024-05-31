d5453ba484f6e0797fb59f80d788f63b06b21702
Kishna Bansal <kb.20051981@gmail.com>
2024-05-31 17:23:50 +0530

    Created the Auth Screen for signup/signin of user and created widgets for Buttons and custom text field

---
+++ lib/common/widgets/custom_button.dart
@@ -0,0 +1,20 @@
+import 'package:amazon_clone_app/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+  const CustomButton({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: GlobalVariables.secondaryColor,
+        minimumSize: const Size(double.infinity, 50),
+      ),
+      child: Text(text,style: const TextStyle(color: Colors.white),),
+    );
+  }
+}
